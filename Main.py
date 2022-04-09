0279b08468c0c79388174d687b762cdb13740eef
John Hourani <jjhourani@gmail.com>
2022-04-09 19:10:15 -0400

    Update Main.py
    Adding comments to code to better document

--- Main.py
+++ Main.py
@@ -9,7 +9,7 @@ APP_KEY = os.environ.get("APP_KEY")
 SHEETY_TOKEN = os.environ.get("SHEETY_TOKEN")
 SHEET_ENDPOINT = os.environ.get("SHEET_ENDPOINT")
 
-
+#  Global Variables
 AGE = 23
 GENDER = "male"
 WEIGHT_KG = 77
@@ -17,6 +17,8 @@ HEIGHT_CM = 176
 
 DATE = str(datetime.now().strftime("%x"))
 TIME = str(datetime.now().strftime("%X"))
+
+#  Nutritionix natural language API
 
 headers = {
     "x-app-id": APP_ID,
@@ -30,9 +32,9 @@ parameters = {
     "age": AGE
 }
 exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
-
 response = requests.post(url=exercise_endpoint, json=parameters, headers=headers)
 
+#  Creating list of dictionaries of the new exercises added
 new_exercises = []
 for exercises in response.json()["exercises"]:
     name = (exercises["name"])
@@ -41,18 +43,22 @@ for exercises in response.json()["exercises"]:
     new_data = {"date": DATE, "time": TIME, "exercise": name, "duration": duration, "calories": cals}
     new_exercises.append(new_data)
 
+    
+#  Usinging Sheety API to connect to a premade Google sheets template. 
+#  See Readme file for steps in setting up sheet template
 headers = {
     "Content-Type": "application/json",
     "Authorization": f"Bearer {SHEETY_TOKEN}"
 }
 
+#  Creating new row in spreadsheet for each workout entered
 for workout in new_exercises:
     new_workout = {"workout":
                    workout
                    }
     request = requests.post(url=SHEET_ENDPOINT, json=new_workout, headers=headers)
 
-print(request.json())
+
 
 
 
